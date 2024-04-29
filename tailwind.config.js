14557569d6979b351393babbb692b55c0ef294d8
Oleh <dudkoleg@gmail.com>
2024-04-29 11:47:22 +0300

    sqlite and html

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//   content: [],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// }
+
+export const content = [
+    "./resources/**/*.html",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+];
+export const theme = {
+    extend: {},
+};
+export const plugins = [];
