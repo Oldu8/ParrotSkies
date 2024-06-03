7e20d5c509cc6305072622325eb8f4587ffa945c
Oleh <dudkoleg@gmail.com>
2024-06-03 09:31:41 +0300

    show post with data and enchanced the styles of inputs

--- resources/views/admin/posts/form.blade.php
+++ resources/views/admin/posts/form.blade.php
@@ -1,22 +1,34 @@
+@php
+    $isNew = isset($post) ? false : true;
+    $method = $isNew ? 'POST' : 'PATCH';
+    $formAction = $isNew ? route('posts.store') : route('posts.update', $post->id);
+@endphp
+
+
 <div class='py-4 font-semibold text-gray-600 mb-2 '>
-    <form action="/admin/posts" method="POST" class="flex flex-col gap-2">
+    <form action="{{ $formAction }}" method="POST" class="flex flex-col gap-2">
         @csrf
+        @if(!$isNew)
+            @method('PATCH')
+        @endif
         <input type="hidden" name="_token" value="{{ csrf_token() }}">
         <div class="input-group">
             <strong>Title:</strong>
-            <input name="title" type="text" placeholder="Title" class="border p-1 rounded" value="{{ old('title') }}">
+            <input name="title" type="text" placeholder="Title" class="border p-1 rounded"
+                value="{{ $post->title ?? old('title') }}" @if(!$isNew) disabled @endif>
         </div>
         <div class="input-group">
             <strong>Description:</strong>
-            <textarea rows="3" name="content" type="text" placeholder="Description" class="border p-1 rounded "
-                value="{{ old('content') }}">
-            </textarea>
+            <textarea rows="3" name="content" placeholder="Description" class="border p-1 rounded" @if(!$isNew) disabled
+            @endif>{{ $post->content ?? old('content') }}</textarea>
+
         </div>
         <div class="input-group">
             <strong>Is Active:</strong>
             <div class="toggle-switch">
                 <input type="hidden" name="active" value="0">
-                <input type="checkbox" id="active" name="active" value="1">
+                <input type="checkbox" id="active" name="active" value="1" @if(isset($post) && $post->active) checked
+                @endif @if(!$isNew) disabled @endif>
                 <label for="active" class="toggle-label">
                     <span class="toggle-inner"></span>
                     <span class="toggle-switch"></span>
@@ -26,39 +38,39 @@
         <div class="grid grid-cols-2 gap-4">
             <div class="input-group">
                 <strong>Select category:</strong>
-                <select name="category_id" class="border p-1 rounded" style="width: 300px">
+                <select name="category_id" class="border p-1 rounded" style="width: 300px" @if(!$isNew) disabled @endif>
                     @foreach($categories as $key => $category)
-                        <option class="border p-1 rounded" value="{{ $key }}">{{ $category }}</option>
+                        <option class="border p-1 rounded" value="{{ $key }}" @if(isset($post) && $post->category_id == $key)
+                        selected @endif>{{ $category }}</option>
                     @endforeach
                 </select>
             </div>
             <div class="input-group">
                 <strong>Select an image:</strong>
-                <input type="text" name="thumbnail" style="width: 300px">
+                <input type="text" name="thumbnail" value="{{ $post->thumbnail ?? '' }}" style="width: 300px"
+                    @if(!$isNew) disabled @endif>
             </div>
             <div class="input-group">
                 <strong>Set your slug:</strong>
-                <input type="text" name="slug" style="width: 300px">
+                <input type="text" name="slug" value="{{ $post->slug ?? '' }}" style="width: 300px" @if(!$isNew)
+                disabled @endif>
             </div>
             <div class="input-group">
                 <strong>Select time of publishing:</strong>
-                <input type="datetime-local" name="published_at" style="width: 300px">
+                <!-- <input type="datetime-local" name="published_at" style="width: 300px"> -->
+                <input type="datetime-local" name="published_at" value="{{ isset($post) ? $post->published_at : '' }}"
+                    style="width: 300px" @if(!$isNew) disabled @endif>
+
             </div>
         </div>
-
-        <hr>
-        @if($errors->any())
-            <div class="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
-                <ul>
-                    @foreach($errors->all() as $error)
-                        <li>{{ $error }}</li>
-                    @endforeach
-                </ul>
-            </div>
-            <hr>
-        @endif
         <div class="pt-6">
-            <input type="submit" value="Create" class="border p-1 rounded bg-blue-500 text-white w-1/4">
+            @if($isNew)
+                <input type="submit" value="Create" class="border p-1 rounded bg-blue-500 text-white w-1/4">
+            @else
+                <button type="button" id="editButton" class="border p-1 rounded bg-blue-500 text-white w-1/4">Edit</button>
+                <input type="submit" value="Save" class="border p-1 rounded bg-blue-500 text-white w-1/4"
+                    style="display: none;" id="saveButton">
+            @endif
         </div>
     </form>
 </div>
