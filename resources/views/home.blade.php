7a1b2e6f7df0254e0f6d17d2030fdbe8d9377084
Oleh <dudkoleg@gmail.com>
2024-06-10 09:48:58 +0300

    upd

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,14 @@
 @extends('layouts.app')
 
+<!-- add pages for posts and categories with simple styles -->
+
 @section('content')
 <div class="container">
     <div>
-        Parrot Skies - is a volunteer project that aims to help people around the world with their parrots. Our goal is to make information about traveling with pets and especially with parrots accessible to most travelers. 
-        We trying to make immigration and traveling with birds more understating and possible, to prevent people will leave their pets. Hope you can find all necessary information on this website or in our small group.
+        Parrot Skies - is a volunteer project that aims to help people around the world with their parrots. Our goal is
+        to make information about traveling with pets and especially with parrots accessible to most travelers.
+        We trying to make immigration and traveling with birds more understating and possible, to prevent people will
+        leave their pets. Hope you can find all necessary information on this website or in our small group.
     </div>
     <div>
         Popular posts
