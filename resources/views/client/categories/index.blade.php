d9e89b0dde0e407be928e32f16b89e5b8dd91a6d
Oleh <dudkoleg@gmail.com>
2024-06-12 10:30:15 +0300

    improved styles

--- resources/views/client/categories/index.blade.php
+++ resources/views/client/categories/index.blade.php
@@ -1,22 +1,22 @@
 @extends('client.layouts.app')
 
-
 @section('content')
-<div class="container">
-    <div>
-        <h1 class="text-3xl font-bold underline">Find posts in needed category</h1>
-    </div>
-    <div>
-        <h2>List of categories:</h2>
-        <ul>
-            @foreach($categories as $key => $category)
-                <li class="border p-1 rounded" value="{{ $key }}">
-                    <a href="/posts?category={{ $key }}" class="hover:text-blue-500">
-                        {{ $category }}
-                    </a>
-                </li>
-            @endforeach
-        </ul>
+<div class="container mx-auto mt-8 px-4 sm:px-6 lg:px-8">
+    <div class="bg-white p-6 rounded-lg shadow-lg">
+        <h1 class="text-4xl font-bold mb-6 text-center text-gray-800">Find posts in needed category</h1>
+        <div class="mb-4">
+            <h2 class="text-2xl font-semibold text-gray-700">List of categories:</h2>
+            <ul class="mt-4">
+                @foreach($categories as $key => $category)
+                    <li class="mb-2">
+                        <a href="/posts?category={{ $key }}"
+                            class="block underline font-semibold text-lg p-3 rounded hover:bg-gray-100 hover:text-blue-500 transition duration-200">
+                            {{ $category }}
+                        </a>
+                    </li>
+                @endforeach
+            </ul>
+        </div>
     </div>
 </div>
-@endSection
+@endsection
