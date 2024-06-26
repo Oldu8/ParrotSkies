a5767747c753109734490e32c9c06bfad32e6942
Oleh <dudkoleg@gmail.com>
2024-06-26 09:41:06 +0300

    new migrations

--- app/Http/Controllers/AdminAuthController.php
+++ app/Http/Controllers/AdminAuthController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\User;
+use App\Models\AdminUser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
@@ -14,42 +14,42 @@ class AdminAuthController extends Controller
         return view('admin.auth.register');
     }
 
-    // public function registerPost(Request $request)
-    // {
-    //     $user = new User();
+    public function registerPost(Request $request)
+    {
+        $user = new AdminUser();
 
-    //     $user->name = $request->name;
-    //     $user->email = $request->email;
-    //     $user->password = Hash::make($request->password);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = Hash::make($request->password);
 
-    //     $user->save();
+        $user->save();
 
-    //     return back()->with('success', 'Register successfully');
-    // }
+        return back()->with('success', 'Register successfully');
+    }
 
     public function login()
     {
         return view('admin.auth.login');
     }
 
-    // public function loginPost(Request $request)
-    // {
-    //     $credetials = [
-    //         'email' => $request->email,
-    //         'password' => $request->password,
-    //     ];
+    public function loginPost(Request $request)
+    {
+        $credentials = [
+            'email' => $request->email,
+            'password' => $request->password,
+        ];
 
-    //     if (Auth::attempt($credetials)) {
-    //         return redirect('/home')->with('success', 'Login Success');
-    //     }
+        if (Auth::attempt($credentials)) {
+            return redirect('/admin/welcome')->with('success', 'Login Success');
+        }
 
-    //     return back()->with('error', 'Error Email or Password');
-    // }
+        return back()->with('error', 'Error Email or Password');
+    }
 
-    // public function logout()
-    // {
-    //     Auth::logout();
+    public function logout()
+    {
+        Auth::logout();
 
-    //     return redirect()->route('login');
-    // }
+        return redirect()->route('login');
+    }
 }
---
+++ app/Models/AdminUser.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class AdminUser extends Model
+{
+    use HasApiTokens, HasFactory, Notifiable;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+        'password' => 'hashed',
+    ];
+}
