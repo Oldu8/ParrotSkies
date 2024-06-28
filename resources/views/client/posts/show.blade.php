2df2ec516cbeeb5683404af138d4f030cfa5849d
Oleh <dudkoleg@gmail.com>
2024-06-28 11:20:51 +0300

    base layout for post fe

--- resources/views/client/posts/show.blade.php
+++ resources/views/client/posts/show.blade.php
@@ -2,8 +2,15 @@
 
 @section('content')
 <div class="container mx-auto mt-12 px-4 sm:px-6 lg:px-8">
-    <div class="bg-white p-8 rounded-lg shadow-lg">
-        <h1 class="text-5xl font-bold mb-8 text-center text-gray-900">{{ $post->title }}</h1>
+    <div class="bg-white p-8 rounded-lg shadow-lg flex flex-col justify-between gap-4  mb-8">
+        <h1 class="text-5xl font-bold text-left text-gray-900">{{ $post->title }}</h1>
+        <p class="text-md text-gray-900">{{ $post->published_at }}</p>
+        <div class="mb-4 w-full max-w-md ">
+            <img src="{{ $post->thumbnail }}" alt="image" />
+        </div>
+    </div>
+    <div class=" p-8">
+        <p class="text-lg text-gray-900">{!! $post->content !!}</p>
     </div>
 </div>
 @endsection
