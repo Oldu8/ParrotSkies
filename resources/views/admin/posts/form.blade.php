1db03d8da053ac028097f3b301e429d681dfc066
Oleh <dudkoleg@gmail.com>
2024-07-05 09:52:55 +0300

    migrations

--- resources/views/admin/posts/form.blade.php
+++ resources/views/admin/posts/form.blade.php
@@ -1,11 +1,9 @@
 @php
-    $isNew = isset($post) ? false : true;
-    $method = $isNew ? 'POST' : 'PATCH';
+    $isNew = !isset($post);
     $formAction = $isNew ? route('posts.store') : route('posts.update', $post->id);
 @endphp
 
-
-<div class='py-4 font-semibold text-gray-600 mb-2 '>
+<div class="py-4 font-semibold text-gray-600 mb-2">
     <form action="{{ $formAction }}" method="POST" class="flex flex-col gap-2">
         @csrf
         @if(!$isNew)
@@ -17,29 +15,19 @@
             <input name="title" type="text" placeholder="Title" id="titleInput" class="border p-1 rounded"
                 value="{{ $post->title ?? old('title') }}" @if(!$isNew) disabled @endif>
         </div>
-        <!-- Get plugin to make data for description editable -->
-        <!-- <div class="input-group">
-            <strong>Description:</strong>
-            <textarea id="contentInput" rows=" 3" name="content" placeholder="Description" class="border p-1 rounded"
-                @if(!$isNew) disabled @endif>{{ $post->content ?? old('content') }}
-            </textarea>
-        </div> -->
         <div class="input-group my-4">
             <strong>Description:</strong>
-            <div id="quillEditor" class="bg-white border p-1 rounded min-h-[200px]" >
+            <div id="quillEditor" class="bg-white border p-1 rounded min-h-[200px]">
                 {!! $post->content ?? old('content') !!}
             </div>
             <input type="hidden" name="content" id="contentInput">
         </div>
-    </form>
         <div class="input-group">
             <strong>Is Active:</strong>
             <div class="toggle-switch">
                 <input type="hidden" name="active" value="0">
-                <input type="checkbox" id="active" name="active" value="1" 
-                @if(isset($post) && $post->active) checked @endif 
-                @if(!$isNew) disabled @endif
-                >
+                <input type="checkbox" id="active" name="active" value="1" @if(isset($post) && $post->active) checked @endif
+                    @if(!$isNew) disabled @endif>
                 <label for="active" class="toggle-label">
                     <span class="toggle-inner"></span>
                     <span class="toggle-switch"></span>
@@ -50,15 +38,13 @@
             <div class="input-group">
                 <strong>Select category:</strong>
                 <select id="categoryInput" name="category_id" class="border p-1 rounded" style="width: 300px"
-                    @if(!$isNew) disabled @endif
-                    >
+                    @if(!$isNew) disabled @endif>
                     @foreach($categories as $key => $category)
                         <option class="border p-1 rounded" value="{{ $key }}" @if(isset($post) && $post->category_id == $key)
                         selected @endif>{{ $category }}</option>
                     @endforeach
                 </select>
             </div>
-            <!-- TODO: make it saves img to storage -->
             <div class="input-group">
                 <strong>Select an image:</strong>
                 <input id="imageInput" type="text" name="thumbnail" value="{{ $post->thumbnail ?? '' }}"
@@ -66,17 +52,14 @@
             </div>
             <div class="input-group">
                 <strong>Set your slug:</strong>
-                <input id="slugInput" type="text" name="slug" value="{{ $post->slug ?? '' }}" style="width: 300px" @if(!$isNew)
-                disabled @endif>
+                <input id="slugInput" type="text" name="slug" value="{{ $post->slug ?? '' }}" style="width: 300px"
+                    @if(!$isNew) disabled @endif>
             </div>
-            <!-- TODO: make this data sets as in DB -->
             <div class="input-group">
                 <strong>Select time of publishing:</strong>
-                <!-- <input type="datetime-local" name="published_at" style="width: 300px"> -->
                 <input id="publishedInput" type="datetime-local" name="published_at"
                     value="{{ isset($post) ? $post->published_at : '' }}" style="width: 300px" @if(!$isNew) disabled
                     @endif>
-
             </div>
         </div>
         <div class="pt-6">
@@ -92,25 +75,34 @@
 </div>
 
 @section('js')
+<script src="https://cdn.quilljs.com/1.3.6/quill.js"></script>
 <script>
-      const quill = new Quill('#quillEditor', {
+    const quill = new Quill('#quillEditor', {
         modules: { toolbar: true },
         placeholder: 'Description',
-        readOnly: true,
-    theme: 'snow'
-  });
-  
+        theme: 'snow',
+        readOnly: {{ $isNew ? 'false' : 'true' }}
+    });
+
+    const form = document.querySelector('form');
+    form.onsubmit = function () {
+        const contentInput = document.querySelector('input[name=content]');
+        contentInput.value = quill.root.innerHTML;
+    };
+
     const editButton = document.getElementById('editButton');
     const saveButton = document.getElementById('saveButton');
-    editButton.addEventListener('click', () => {
-        editButton.style.display = 'none';
-        saveButton.style.display = 'block';
-        document.getElementById('titleInput').disabled = false;
-        document.getElementById('contentInput').disabled = false;
-        document.getElementById('categoryInput').disabled = false;
-        document.getElementById('imageInput').disabled = false;
-        document.getElementById('slugInput').disabled = false;
-        document.getElementById('publishedInput').disabled = false;
-    })
+    if (editButton) {
+        editButton.addEventListener('click', () => {
+            editButton.style.display = 'none';
+            saveButton.style.display = 'block';
+            document.getElementById('titleInput').disabled = false;
+            quill.enable();
+            document.getElementById('categoryInput').disabled = false;
+            document.getElementById('imageInput').disabled = false;
+            document.getElementById('slugInput').disabled = false;
+            document.getElementById('publishedInput').disabled = false;
+        });
+    }
 </script>
-@endSection
+@endsection
